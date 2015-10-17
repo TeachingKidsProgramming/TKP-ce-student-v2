0e0cc94338558f0287ad49df298160c6844a19d1
Anonymous <anonymous@noemail.com>
2015-10-17 23:35:15 +0000

    add

--- src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -1,20 +1,27 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.windows.MessageBox;
 
 public class Spiral
 {
   public static void main(String[] args)
   {
     //    Show the tortoise --#1
-    //    test
     //    Make the tortoise go as fast as possible --#4
-    //    Add Blue Violet to the Color Wheel (HINT: Use PenColors)--#7
+    //    Add Blue Violet to the Color Wheel --#7
     //    Add Violet to the Color Wheel --#8
     //    Add Purple to the Color Wheel --#9
     //    Do the following 75 times --#3.1
-    //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
-    //         Move the tortoise 5 times the current line number you are drawing --#5
-    //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+    try
+    {
+      //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
+      //         Move the tortoise 5 times the current line number you are drawing --#5
+      //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+    }
+    catch (RuntimeException re)
+    {
+      MessageBox.showMessage("Hold up: " + re);
+    }
     //    Repeat --#3.2
   }
 }
